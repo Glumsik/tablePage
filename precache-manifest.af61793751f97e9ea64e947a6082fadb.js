db2fee71302d7f443e7bced927904af56f227dd8
Glumsik <glumsik@icloud.com>
2020-02-08 23:29:29 +0300

    build
    build

---
+++ precache-manifest.af61793751f97e9ea64e947a6082fadb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b5c6fdd8375c07bef6717125970d6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2824c4a9a3d3debc34cf",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "bbc13b1bea15aa475ad1",
+    "url": "/static/css/main.dbdee88a.chunk.css"
+  },
+  {
+    "revision": "2824c4a9a3d3debc34cf",
+    "url": "/static/js/2.d04ad307.chunk.js"
+  },
+  {
+    "revision": "d785ebb2c79e64f251dc6e6e5513ce2d",
+    "url": "/static/js/2.d04ad307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc13b1bea15aa475ad1",
+    "url": "/static/js/main.be459fba.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
